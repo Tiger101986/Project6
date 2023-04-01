e0ae8af5245850025e8f53ac4a8407ba63e69d52
Tiger101986 <kanharobopha@yahoo.com>
2023-03-31 21:03:42 -0700

    Initial creating sauces.js in controllers folder with exports method to make router file easily to understand

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,107 @@
+//import models folder 
+const Thing = require('../models/thing');
+
+exports.createThing = (req, res, next) => {
+    const thing = new Thing({
+        userId: req.body.userId,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        mainPepper: req.body.mainPepper,
+        imageUrl: req.body.imageUrl,
+        heat: req.body.heat,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        usersLiked: req.body.usersLiked,
+        usersDisliked: req.body.usersDisliked,
+    });
+    thing.save()
+        .then(() => {
+            res.status(201).json({
+                message: 'Post saved successfully!'
+            });
+        })
+        .catch(
+            (error) => {
+                res.status(400).json({
+                    error: error
+                });
+            }
+        );
+};
+
+// Retrieving a Specific Thing - Single sauce with .get() and findOne() methods
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({
+        _id: req.params.id
+    }).then((things) => {
+        res.status(200).json(things);
+    }).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );        
+};
+
+//Update an existing Thing with .put() and updateOne() methods
+exports.modifyThing = (req, res, next) => {
+    const thing = new Thing({
+        _id: req.params.id,
+        userId: req.body.userId,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        mainPepper: req.body.mainPepper,
+        imageUrl: req.body.imageUrl,
+        heat: req.body.heat,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        usersLiked: req.body.usersLiked,
+        usersDisliked: req.body.usersDisliked,
+    });
+    Thing.updateOne({ _id: req.params.id }, thing).then(
+        () => {
+            res.status(201).json({
+                message: 'Thing updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
+
+//Delete a Thing with .delete() and deleteOne() methods
+exports.deleteThing =(req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id }).then(
+        () => {
+            res.status(200).json({
+                message: 'Deleted!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
+
+// Retrieving a list of Thing - Array sauces
+exports.getAllThings = (req, res, next) => {
+    Thing.find().then((things) => {
+        res.status(200).json(things);
+    }).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,10 +1,7 @@
 //import express to set up route with Router() method
 const express = require('express');
-const { route } = require('../app');
 const router = express.Router();
-
-//import models folder 
-const Thing = require('../models/thing');
+const sauceControllers = require('../controllers/sauces');
 
 /**
  * add some middleware 
@@ -12,114 +9,25 @@ const Thing = require('../models/thing');
  * Use save() method to save Thing from .post() user input
  * Use find() method to get all Things in Database from .get()
  * Use findOne() method to gets specific/single one Thing from .get()
+ * Use updateOne() method to modify an existing Thing from .put()
+ * Use deleteOne() method to delete a Thing from .delete()
  */
 
 // Replace app to router and each segment (api/sauces) to (/) instead after cut all of routes from app.js file to put inside sauces.js router
-router.post('/', (req, res, next) => {
-    const thing = new Thing({
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-        likes: req.body.likes,
-        dislikes: req.body.dislikes,
-        usersLiked: req.body.usersLiked,
-        usersDisliked: req.body.usersDisliked,
-    });
-    thing.save()
-        .then(() => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            });
-        })
-        .catch(
-            (error) => {
-                res.status(400).json({
-                    error: error
-                });
-            }
-        );
-});
+//Get and save data from user input 
+router.post('/', sauceControllers.createThing);
 
 // Retrieving a Specific Thing - Single sauce with .get() and findOne() methods
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
-    }).then((things) => {
-        res.status(200).json(things);
-    })
-        .catch(
-            (error) => {
-                res.status(400).json({
-                    error: error
-                });
-            }
-        );
-});
+router.get('/:id', sauceControllers.getOneThing);
 
 //Update an existing Thing with .put() and updateOne() methods
-router.put('/:id', (req, res, next) => {
-    const thing = new Thing({
-        _id: req.params.id,
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-        likes: req.body.likes,
-        dislikes: req.body.dislikes,
-        usersLiked: req.body.usersLiked,
-        usersDisliked: req.body.usersDisliked,
-    });
-    Thing.updateOne({ _id: req.params.id }, thing).then(
-        () => {
-            res.status(201).json({
-                message: 'Thing updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
+router.put('/:id', sauceControllers.modifyThing);
 
 //Delete a Thing with .delete() and deleteOne() methods
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: 'Deleted!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
+router.delete('/:id', sauceControllers.deleteThing);
 
 // Retrieving a list of Thing - Array sauces
-router.get('/', (req, res, next) => {
-    Thing.find().then((things) => {
-        res.status(200).json(things);
-    }).catch(
-        (error) => {
-            res.status(400).json({
-                error: error
-            });
-        }
-    );
-});
+router.get('/', sauceControllers.getAllThings);
 
 //export router
 module.exports = router;
